31c4a32ae395143ebcecca98fd6315ab8a93836f
Michael Köcher <michael_koecher@web.de>
2017-12-14 23:49:34 +0100

    Tag 4 komplett

--- Day4.py
+++ Day4.py
@@ -4,6 +4,25 @@ def compare_words(word1, word2):
 
       return bool(not (word1 == word2))
 
+def compare_characters(word1, word2):
+
+    if len(word1) == len(word2):
+        print(word1 + ' ' + word2)
+        for char in word1:
+            if count_character(word1, char) != count_character(word2, char):
+                return True
+        return False
+
+    return True
+
+def count_character(word, character):
+
+    count = 0
+    for char in word:
+        if char == character:
+            count += 1
+    return count
+
 def Day4(check_words):
 
     sum = 0
@@ -26,3 +45,4 @@ def Day4(check_words):
     return sum
 
 print(Day4(compare_words))
+print(Day4(compare_characters))
