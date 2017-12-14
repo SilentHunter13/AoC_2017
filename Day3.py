5ff9210677c74e13d43a30d0225a9fd55a48ba37
Michael Köcher <michael_koecher@web.de>
2017-12-14 22:24:04 +0100

    Tag 3 komplett

--- Day3.py
+++ Day3.py
@@ -33,28 +33,44 @@ def plus_one(old_value, matrix, point):
 
     return old_value + 1
 
+def add_neighbours(old_value, matrix, point):
+
+    new_value = get_index(matrix, point)
+    new_value += get_index(matrix, point + np.array([(1),(0)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(1),(1)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(0),(1)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(-1),(1)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(-1),(0)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(-1),(-1)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(0),(-1)], dtype = int).reshape(2,1))
+    new_value += get_index(matrix, point + np.array([(1),(-1)], dtype = int).reshape(2,1))
+
+    return new_value
+
 def Day3(puzzle_input, get_new_value):
 
-    value = 0
+    value = 1
     point = np.zeros([2,1], dtype = int)
     direction = np.array([(1),(0)], dtype = int).reshape(2,1)
     matrix = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype = int)
+    set_index(matrix, point, value)
 
-    while value < puzzle_input:
-        #Wert erhöhen
-        value = get_new_value(value, matrix, point)
-
+    while value <= puzzle_input:
         #Wert schreiben
         set_index(matrix, point, value)
-
-        #neue Richtung berechnen
-        direction = get_direction(matrix, point, direction)
 
         #in die Richtung weitergehen
         point = point + direction
 
+        #Wert erhöhen
+        value = get_new_value(value, matrix, point)
+
+        #neue Richtung berechnen
+        direction = get_direction(matrix, point, direction)
+
     #letzen Schritt zurücknehmen
     point = point - direction
-    return abs(point[0,0]) + abs(point[1,0])
+    return (abs(point[0,0]) + abs(point[1,0]), value)
 
 print(Day3(361527, plus_one))
+print(Day3(361527, add_neighbours))
