0ac7a06072c113b7baedca557686c10354c91e3c
Michael Köcher <michael_koecher@web.de>
2018-01-02 14:29:04 +0100

    Tag 12 komplett

--- Day12.py
+++ Day12.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
-def decode_connection(join_mat, line):
+def decode_connection(groups, line):
 
     parts = line.split('<->')
 
     partners = parts[1].split(',')
-
-    for partner in partners:
-
-        join_mat[int(parts[0]), int(partner)] = True
+    
+    partners.append(parts[0])
+    
+    groups.append(frozenset([int(x) for x in partners]))
 
 def day12():
 
-    join_mat = np.zeros((2000,2000), dtype = bool)
+    groups = list()
 
     with open('./Input/Day12.txt', 'r') as file:
 
         for line in file.readlines():
-            decode_connection(join_mat, line)
+            decode_connection(groups, line)
 
     ready = False
 
     while not ready:
-
-        old = np.array(join_mat[0,:])
-        for index, row in enumerate(join_mat):
-            if index in list(np.where(join_mat[0,:] == True)[0]):
-                join_mat[0,:] = join_mat[0,:] | row
-
-        if (old == join_mat[0,:]).all():
-            ready = True
-
-
-    return np.sum(join_mat[0,:])
+        ready = True
+        for group1 in groups:
+            for group2 in [x for x in groups if group1 is not x]:
+                if not group1.isdisjoint(group2):
+                    index1 = groups.index(group1)
+                    groups[index1] = group1.union(group2)
+                    group1 = group1.union(group2)
+                    del(groups[groups.index(group2)])
+                    ready = False
+    
+    for group in groups:
+        if 0 in group:
+            group0_length = len(group)
+    
+    return (group0_length, len(groups))
 
 print(day12())
