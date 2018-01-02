741b5d6d2d25096a3ae0a072df48bc3aefb899f2
Michael Köcher <michael_koecher@web.de>
2018-01-02 19:05:36 +0100

    Tag10 komplett

--- Day10.py
+++ Day10.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-def day10(elements, lengths):
+LIST_LENGTH = 256
+
+def day10_p1(lengths):
     
-    old_list = range(elements)
+    old_list = range(LIST_LENGTH)
     current_position = 0
     skip_size = 0
     
@@ -10,8 +12,8 @@ def day10(elements, lengths):
         
         new_list = list(old_list)
         for i in range(length):
-            old_position = (current_position + i) % elements
-            new_position = (current_position + length - 1 - i) % elements
+            old_position = (current_position + i) % LIST_LENGTH
+            new_position = (current_position + length - 1 - i) % LIST_LENGTH
             new_list[new_position] = old_list[old_position]
         
         old_list = new_list
@@ -19,5 +21,43 @@ def day10(elements, lengths):
         skip_size += 1
             
     return old_list[0] * old_list[1]
+    
+def knot_hash(string):
 
-print(day10(256, [147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70]))
+    lengths = [ord(x) for x in string]
+    
+    lengths += [17, 31, 73, 47, 23]
+    
+    old_list = range(LIST_LENGTH)
+    current_position = 0
+    skip_size = 0
+    
+    for _ in range(64):
+        for length in lengths:
+        
+            new_list = list(old_list)
+            for i in range(length):
+                old_position = (current_position + i) % LIST_LENGTH
+                new_position = (current_position + length - 1 - i) % LIST_LENGTH
+                new_list[new_position] = old_list[old_position]
+        
+            old_list = new_list
+            current_position += length + skip_size
+            skip_size += 1
+        
+    dense_hash = ''
+    
+    for byte_index in range(16):
+    
+        dense_hash_byte = 0
+    
+        for index in range(16):
+        
+            dense_hash_byte ^= old_list[(16 * byte_index) + index]
+        
+        dense_hash += '{:02x}'.format(dense_hash_byte)
+    
+    return dense_hash
+
+print(day10_p1([147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70]))
+print(knot_hash('147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70'))
