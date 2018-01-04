03eb4eaed9041de78c1b4d66b197344c0cfaccd0
Michael Köcher <michael_koecher@web.de>
2018-01-04 16:30:35 +0100

    Tag 16 Teil 1

--- Day16.py
+++ Day16.py
@@ -9,13 +9,21 @@ def day16():
 
         for line in file.readlines():
             moves = line.split(',')
-            
+
             for move in moves:
                 if move[0] == 's':
-                    dance_line = dance_line[
+                    count = int(move[1:])
+                    if count > 0:
+                        dance_line = dance_line[-count:] + dance_line[:len(dance_line) - count]
                 elif move[0] == 'x':
-                
+                    positions = [int(x) for x in move[1:].split('/')]
+                    dance_line[positions[0]], dance_line[positions[1]] = dance_line[positions[1]], dance_line[positions[0]]
                 elif move[0] == 'p':
-            
+                    partners = move[1:].split('/')
+                    index = [dance_line.index(x) for x in partners]
+                    dance_line[index[0]], dance_line[index[1]] = dance_line[index[1]], dance_line[index[0]]
+
+    return ''.join(dance_line)
+
 
 print(day16())
