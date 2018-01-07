ac907aeaa04581814c677808e852521eb69e2409
Michael Köcher <michael_koecher@web.de>
2018-01-07 18:52:49 +0100

    Trennung zwischen p und sx Befehlen begonnen
    sx Befehle laufen jetzt getrennt

--- Day16.py
+++ Day16.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import cProfile
-import array
 
 SPIN = 0
 EXCHANGE = 1
 PARTNER = 2
 
-LINE_COUNT = 16
+LINE_WIDTH = 5
 
 def bits(num):
 
@@ -15,40 +14,69 @@ def bits(num):
         yield bool(num & 0x01)
         num = num >> 1
 
-def read_moves(line):
+class Dancer:
 
-    moves = list()
-    for str_move in line.split(','):
-        str_move.strip()
-        if str_move[0] == 's':
-            move = {'type': SPIN, 'value1': int(str_move[1:])}
-        elif str_move[0] == 'x':
-            move = {'type': EXCHANGE, 'value1': int(str_move[1:].split('/')[0]), 'value2': int(str_move[1:].split('/')[1])}
-        elif str_move[0] == 'p':
-            move = {'type': PARTNER, 'value1': ord(str_move[1:].split('/')[0]) - 0x61, 'value2': ord(str_move[1:].split('/')[1]) - 0x61}
+    def __init__(self, filename):
+        self.sx_lookup = list(range(LINE_WIDTH))
+        #self.p_lookup = list(range(LINE_WIDTH))
 
-        moves.append(move)
+        self.char_line = list([x for x in 'abcdefghijklmnop'])
 
-    return moves
+        with open(filename, 'r') as file:
 
-def dance(dance_line, moves):
-    new_dance_line = dance_line[:]
-    for move in moves:
-        if move['type'] == SPIN:
-            if move['value1'] > 0:
-                new_dance_line = new_dance_line[-move['value1']:] + new_dance_line[:LINE_COUNT - move['value1']]
-        elif move['type'] == EXCHANGE:
-            new_dance_line[move['value1']], new_dance_line[move['value2']] = new_dance_line[move['value2']], new_dance_line[move['value1']]
-        elif move['type'] == PARTNER:
-            index1 = new_dance_line.index(move['value1'])
-            index2 = new_dance_line.index(move['value2'])
-            new_dance_line[index1], new_dance_line[index2] = new_dance_line[index2], new_dance_line[index1]
+            for line in file.readlines():
+                self.read_moves(line)
 
-    return new_dance_line
+    def read_moves(self, line):
+
+        for str_move in line.split(','):
+            str_move = str_move.strip()
+            if str_move[0] == 's':
+                move = {'type': SPIN, 'value1': int(str_move[1:])}
+                spin_count = int(str_move[1:])
+                if spin_count > 0:
+                    self.sx_lookup = self.sx_lookup[-spin_count:] + self.sx_lookup[:LINE_WIDTH - spin_count]
+            elif str_move[0] == 'x':
+                move = {'type': EXCHANGE, 'value1': int(str_move[1:].split('/')[0]), 'value2': int(str_move[1:].split('/')[1])}
+                pos1 = int(str_move[1:].split('/')[0])
+                pos2 = int(str_move[1:].split('/')[1])
+                self.sx_lookup[pos1], self.sx_lookup[pos2] = self.sx_lookup[pos2], self.sx_lookup[pos1]
+            elif str_move[0] == 'p':
+                move = {'type': PARTNER, 'value1': ord(str_move[1:].split('/')[0]) - 0x61, 'value2': ord(str_move[1:].split('/')[1]) - 0x61}
+
+
+#def dance(dance_line, moves):
+#    new_dance_line = dance_line[:]
+#    for move in moves:
+#        if move['type'] == SPIN:
+#            if move['value1'] > 0:
+#                new_dance_line = new_dance_line[-move['value1']:] + new_dance_line[:LINE_WIDTH - move['value1']]
+#        elif move['type'] == EXCHANGE:
+#            new_dance_line[move['value1']], new_dance_line[move['value2']] = new_dance_line[move['value2']], new_dance_line[move['value1']]
+#        elif move['type'] == PARTNER:
+#            index1 = new_dance_line.index(move['value1'])
+#            index2 = new_dance_line.index(move['value2'])
+#            new_dance_line[index1], new_dance_line[index2] = new_dance_line[index2], new_dance_line[index1]
+#
+#    return new_dance_line
+
+    def dance(self, dance_count):
+        sx_template = self.sx_lookup.copy()
+        sx_line = list(range(LINE_WIDTH))
+        for bit in bits(dance_count):
+            #print(template_line)
+            if bit:
+                sx_line = dance_on(sx_line, sx_template)
+
+            #neues sx Template berechnen
+            sx_template = dance_on(sx_template, sx_template)
+            print(sx_template)
+
+        return sx_line
 
 def dance_on(dance_line, template):
 
-    new_dance_line = dance_line[:]
+    new_dance_line = dance_line.copy()
     for index, element in enumerate(dance_line):
         new_dance_line[index] = dance_line[template[index]]
 
@@ -56,30 +84,19 @@ def dance_on(dance_line, template):
 
 def day16(dance_count):
 
-    char_line = list([x for x in 'abcdefghijklmnop'])
-    dance_line = array.array('I', range(LINE_COUNT))
+    dancer = Dancer('./Input/Day16_t1.txt')
 
-    with open('./Input/Day16.txt', 'r') as file:
 
-        for line in file.readlines():
-            moves = read_moves(line)
-
-        template_line = dance(dance_line, moves)
-
-        for bit in bits(dance_count):
-            if bit:
-                print(dance_line)
-                dance_line = dance_on(dance_line, template_line)
-
-            #neue Templates berechnen
-            template_line = dance_on(template_line, template_line)
+        #template_line = dance(dance_line, moves)
 
 #        for _ in range(dance_count):
 #            dance_line = dance(dance_line, moves)
-    print(dance_line)
-    return ''.join([char_line[c] for c in dance_line])
+#            print(dance_line)
+#    return ''.join([char_line[c] for c in dance_line])
+    return dancer.dance(dance_count)
 
 
-print(day16(1))
+print(day16(3))
 #cProfile.run('day16(2)')
-#print(day16(1000000000))
+#print(day16(2))
+#print(day16(3))
